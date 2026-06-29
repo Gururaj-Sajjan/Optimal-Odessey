c2e56f2d06d5a50333c7930f79010ce1a79a6dd6
Gururaj-Sajjan <gururajsajjan813@gmail.com>
2026-06-29 21:08:30 +0530

    DID

--- Week1/Building_Roads.cpp
+++ Week1/Building_Roads.cpp
@@ -1,12 +1,51 @@
 #include<iostream>
 #include<vector>
+#include<tuple>
 using namespace std;
+void visit(int b,vector<vector<int>>& roads,vector<bool>& tf){
+    if(tf[b]) return;
+    tf[b]=true;
+    for(int i=0;i<roads[b].size();i++){
+        visit(roads[b][i],roads,tf);
+    }
+    return;
+}
+void find(vector<vector<int>>& roads,vector<bool>& tf){
+    for(int j=0;j<roads[1].size();j++){
+        visit(roads[1][j],roads,tf);
+    }
+    return;
+}
+void change(vector<vector<int>>& roads, vector<pair<int,int>>& nroads,vector<bool>& tf){
+    for(int i=2;i<tf.size();i++){
+        if(tf[i]) continue;
+        roads[1].push_back(i);
+	    roads[i].push_back(1);
+        nroads.push_back({1,i});
+        visit(i,roads,tf);
+    }
+}
 int main(){
     int n,m;
-    vector<pair<int,int>>roads(m);
     cin>>n>>m;
+    int a,b;
+    vector<pair<int,int>>nroads;
+    vector<vector<int>>roads(n+1);
+    vector<bool>tf(n+1);
+    tf[0]=false;
+    tf[1]=true;
+    for(int i=0;i<n+1;i++){
+	    tf[i]=false;
+	}
     for(int i=0;i<m;i++){
-        cin>>roads[i].first>>roads[i].second;
+        cin>>a>>b;
+        roads[a].push_back(b);
+        roads[b].push_back(a);
     }
-    
+    find(roads,tf);
+    change(roads,nroads,tf);
+    cout<<nroads.size()<<endl;
+    for(int i=0;i<nroads.size();i++){
+        cout<<nroads[i].first<<" "<<nroads[i].second<<endl;
+    }
 }
--- Week1/labyrinth.cpp
+++ Week1/labyrinth.cpp
@@ -3,14 +3,15 @@
 #include<vector>
 #include<string>
 using namespace std;
-string find(int i1,int j1,int i2,int j2,int n,int m,int y,bool& uni,int x,string s);
+string find(int i1,int j1,int i2,int j2,int n,int m,int y,bool& uni,int x,string s){
+    
+}
 bool ans(vector<vector<bool>>& need,int n,int m,int i1,int j1,int i2,int j2,vector<char>& p,int& x,bool& uni){
      int y=0;
      string s,s1,s2,s3,s4;
      if(i1-1==i2 && j1==j2){
         x=1;
-        return true;
-     }
+        return true;}
      if(i1+1==i2 && j1==j2){
         x=1;
         return true;
